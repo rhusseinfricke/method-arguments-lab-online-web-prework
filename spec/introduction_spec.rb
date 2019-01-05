270c863a529bc00f3a9f3650eb15f45c5d57884e
boisterous-design-4553 <reemimhussein@outlook.com>
2019-01-05 08:12:26 +0000

    Done.

--- spec/introduction_spec.rb
+++ spec/introduction_spec.rb
@@ -6,8 +6,8 @@ describe "#introduction" do
   end
 end
 
-describe "#introduction_with_language" do 
+describe(name,language) "#introduction_with_language" do 
   it "takes in two arguments, a name and a language, and puts out a phrase using those arguments." do 
-    expect{introduction_with_language("Dan", "Ember.js")}.to output("Hi, my name is Dan and I am learning to program in Ember.js.\n").to_stdout
+    expect{introduction_with_language("Dan", "Ember.js")}.to output("Hi, my name is "Dan" and I am learning to program in "Ember.js".\n").to_stdout
   end
 end
